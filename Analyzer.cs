b08275cd8503c6f48659f6c1ba85a5decf093fd0
Maxim Khafaev <133359009+maximkhafaev@users.noreply.github.com>
2023-05-15 14:52:16 +0700

    Add files via upload

---
+++ Analyzer.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tf9ik
+{
+    public static class Analyzer
+    {
+        public class Error
+        {
+            public int Position;
+            public string Message;
+            public Error(int _position, string _message)
+            {
+                Position = _position;
+                Message = _message;
+            }
+        }
+
+        private static List<Error> errors;
+        private static StringBuilder result;
+        private static int index;
+        private static int lastIndex;
+        private static List<Leksem> leksems;
+
+        private static void SkipDelimiter()
+        {
+            while (index < leksems.Count && leksems[index].Type == LeksemType.Delimiter)
+                index++;
+        }
+
+        private static void SkipDelimiterAndUnknown()
+        {
+            while (index < leksems.Count && (leksems[index].Type == LeksemType.Delimiter || leksems[index].Type == LeksemType.Unknown))
+                index++;
+        }
+
+        private static bool SkipWhile(LeksemType type)
+        {
+            int count = 0;
+            var tmpIndex = index;
+            while (tmpIndex < leksems.Count && leksems[tmpIndex].Type != type)
+            {
+                if (leksems[tmpIndex].Type != LeksemType.Delimiter)
+                    count++;
+                tmpIndex++;
+            }
+
+            if (count == 0 && tmpIndex != leksems.Count)
+                return true;
+
+            if (tmpIndex < leksems.Count && leksems[tmpIndex].Type == type)
+                index = tmpIndex;
+
+            return false;
+        }
+
+        private static bool SkipWhile2(LeksemType type1, LeksemType type2)
+        {
+            int count = 0;
+
+            var tmpIndex = index;
+            while (tmpIndex < leksems.Count && leksems[tmpIndex].Type != type1 && leksems[tmpIndex].Type != type2)
+            {
+                if (leksems[tmpIndex].Type != LeksemType.Delimiter)
+                    count++;
+                tmpIndex++;
+            }
+
+            if (count == 0 && tmpIndex != leksems.Count)
+                return true;
+
+            if (tmpIndex < leksems.Count && tmpIndex < leksems.Count)
+                index = tmpIndex;
+
+            return false;
+        }
+
+        public static string GetResult(List<Leksem> _leksems)
+        {
+            index = 0;
+            leksems = _leksems;
+            if (leksems.Count == 0)
+                lastIndex = 0;
+            else
+                lastIndex = leksems[leksems.Count - 1].Index;
+            errors = new List<Error>();
+
+            Operator();
+
+            if (errors.Count == 0)
+                return "Ошибок нет";
+
+            result = new StringBuilder();
+            foreach (var error in errors)
+            {
+                result.AppendLine($"Ошибка. Позиция {error.Position + 1}: {error.Message}");
+            }
+            return result.ToString();
+        }
+
+        private static void Operator()
+        {
+            var previousIndex = 0;
+            while (index < leksems.Count && leksems[index].Type == LeksemType.Delimiter) index++;
+
+            if (index >= leksems.Count || leksems[index].Type != LeksemType.Printf)
+            {
+                errors.Add(new Error(index < leksems.Count ? leksems[index].Index : lastIndex, "Отсутствует printf"));
+            }
+
+            index++;
+
+            previousIndex = index;
+            if (!SkipWhile(LeksemType.OpeningBracket) && index < leksems.Count && leksems[index].Type == LeksemType.OpeningBracket)
+            {
+                errors.Add(new Error(previousIndex < leksems.Count ? leksems[previousIndex].Index : lastIndex, "Встречен символ, отличный от открывающей скобки"));
+                index++;
+            }
+            else if (index >= leksems.Count || leksems[index].Type != LeksemType.OpeningBracket)
+            {
+                errors.Add(new Error(previousIndex < leksems.Count ? leksems[previousIndex].Index : lastIndex, "Не найдена открывающая скобка"));
+            }
+            else
+            {
+                index++;
+            }
+
+            String();
+
+            Argument();
+
+
+            previousIndex = index;
+
+            if (index < leksems.Count && !SkipWhile(LeksemType.ClosingBracket) && index != previousIndex )
+                errors.Add(new Error(previousIndex < leksems.Count ? leksems[previousIndex].Index : lastIndex, "Неверный формат аргумента"));
+
+            if (index >= leksems.Count || leksems[index].Type != LeksemType.ClosingBracket)
+            {
+                errors.Add(new Error(index < leksems.Count ? leksems[index].Index : lastIndex, "Не найдена закрывающая скобка"));
+            }
+            else
+            {
+                index++;
+            }
+
+            SkipWhile(LeksemType.Semicolon);
+
+            if (index >= leksems.Count || leksems[index].Type != LeksemType.Semicolon)
+            {
+                errors.Add(new Error(index < leksems.Count ? leksems[index].Index : lastIndex, "Не найдена точка с запятой"));
+                index++;
+            }
+        }
+
+        private static void String()
+        {
+            SkipDelimiterAndUnknown();
+            if (index >= leksems.Count || leksems[index].Type != LeksemType.QuotationMarks)
+            {
+                errors.Add(new Error(index < leksems.Count ? leksems[index].Index : lastIndex, "Не найдены открывающие кавычки"));
+            }
+            else
+            {
+                index++;
+            }
+
+            while (index < leksems.Count && (leksems[index].Type == LeksemType.Word ||
+                leksems[index].Type == LeksemType.Delimiter || leksems[index].Type == LeksemType.Specifier))
+            {
+                index++;
+            }
+
+            var previousIndex = index;
+            if (!SkipWhile(LeksemType.QuotationMarks) && leksems[index].Type == LeksemType.QuotationMarks)
+            {
+                errors.Add(new Error(index, "Неверный формат строки"));
+            }
+            if (index >= leksems.Count || leksems[index].Type != LeksemType.QuotationMarks)
+            {
+                errors.Add(new Error(previousIndex < leksems.Count ? leksems[previousIndex].Index : lastIndex, "Не найдены закрывающие кавычки"));
+            }
+            else
+            {
+                index++;
+            }
+        }
+
+        private static void Argument()
+        {
+            SkipDelimiter();
+            while (index < leksems.Count && leksems[index].Type == LeksemType.Comma)
+            {
+                index++;
+                SkipDelimiter();
+                var previousIndex = index;
+                if (index < leksems.Count)
+                {
+                    if (!SkipWhile2(LeksemType.Id, LeksemType.Number))
+                    {
+                        errors.Add(new Error(previousIndex < leksems.Count ? leksems[previousIndex].Index : lastIndex, "Неверный формат аргумента"));
+                        if (index < leksems.Count && (leksems[index].Type == LeksemType.Id || leksems[index].Type == LeksemType.Number))
+                            index++;
+                    }
+                    else
+                    {
+                        index++;
+                    }
+                }
+                SkipDelimiter();
+            }
+        }
+    }
+}
---
+++ Scaner.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tf9ik
+{
+    public class Leksem
+    {
+        public Leksem(int index, LeksemType type)
+        {
+            Index = index;
+            Type = type;
+        }
+        public int Index { get; set; }
+        public LeksemType Type { get; set; }
+    }
+    public enum LeksemType
+    {
+        Printf,
+        QuotationMarks,
+        Word,
+        Delimiter,
+        Specifier,
+        Id,
+        Number,
+        Unknown,
+        OpeningBracket,
+        ClosingBracket,
+        Comma,
+        Semicolon
+    }
+    static internal class Scaner
+    {
+        private static string str;
+        public static bool isPrintf(int index)
+        {
+            if (str.Length - index < 6)
+                return false;
+            if (str.Substring(index, 6) == "printf")
+                return true;
+            return false;
+        }
+
+        public static List<Leksem> GetLeksem(string s)
+        {
+            bool afterComma = false;
+            str = s;
+            var listLeksem = new List<Leksem>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsWhiteSpace(str[i]))
+                {
+                    listLeksem.Add(new Leksem(i, LeksemType.Delimiter));
+                    continue;
+                }
+
+                if (isPrintf(i))
+                {
+                    listLeksem.Add(new Leksem(i, LeksemType.Printf));
+                    i += 5;
+                    continue;
+                }
+
+                if (str[i] == '"')
+                {
+                    listLeksem.Add(new Leksem(i, LeksemType.QuotationMarks));
+                    continue;
+                }
+
+                if (str[i] == '%')
+                {
+                    if (str.Length > i + 1 && (str[i + 1] == 'c' || str[i + 1] == 'd' | str[i + 1] == 'i'))
+                    {
+                        listLeksem.Add(new Leksem(i, LeksemType.Specifier));
+                        i++;
+                        continue;
+                    }
+                    listLeksem.Add(new Leksem(i, LeksemType.Unknown));
+                }
+
+                if (str[i] == '%')
+                {
+                    if (str.Length > i + 1 && (str[i + 1] == 'c' || str[i + 1] == 'd' | str[i + 1] == 'i'))
+                    {
+                        listLeksem.Add(new Leksem(i, LeksemType.QuotationMarks));
+                        i++;
+                        continue;
+                    }
+                    listLeksem.Add(new Leksem(i, LeksemType.Unknown));
+                }
+
+                if (Char.IsLetterOrDigit(str[i]) && !afterComma)
+                {
+                    var startIndex = i;
+                    while (i + 1 < str.Length && Char.IsLetterOrDigit(str[i + 1]))
+                    {
+                        i++;
+                    }
+                    listLeksem.Add(new Leksem(startIndex, LeksemType.Word));
+                    continue;
+                }
+
+                if (Char.IsLetter(str[i]) && afterComma)
+                {
+                    var startIndex = i;
+                    while (i + 1 < str.Length && Char.IsLetterOrDigit(str[i + 1]))
+                    {
+                        i++;
+                    }
+                    listLeksem.Add(new Leksem(startIndex, LeksemType.Id));
+                    continue;
+                }
+
+                if (Char.IsNumber(str[i]))
+                {
+                    var startIndex = i;
+                    while (i + 1 < str.Length && Char.IsNumber(str[i + 1]))
+                    {
+                        i++;
+                    }
+                    listLeksem.Add(new Leksem(startIndex, LeksemType.Number));
+                    continue;
+                }
+
+                if (str[i] == '(')
+                {
+                    listLeksem.Add(new Leksem(i, LeksemType.OpeningBracket));
+                    continue;
+                }
+
+                if (str[i] == ')' )
+                {
+                    listLeksem.Add(new Leksem(i, LeksemType.ClosingBracket));
+                    continue;
+                }
+
+                if (str[i] == ',' )
+                {
+                    listLeksem.Add(new Leksem(i, LeksemType.Comma));
+                    afterComma = true;
+                    continue;
+                }
+
+                if (str[i] == ';' )
+                {
+                    listLeksem.Add(new Leksem(i, LeksemType.Semicolon));
+                    continue;
+                }
+
+                listLeksem.Add(new Leksem(i, LeksemType.Unknown));
+            }
+            return listLeksem; ;
+        }
+    }
+}
+
